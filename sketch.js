373368fc667463b1ea6bf52be7ff5ba0dc606e3a
MaazAnis <73882627+MaazAnis@users.noreply.github.com>
2021-02-18 01:09:25 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var balloon;
+
+var database;
+
+function setup() {
+  createCanvas(500,500);
+  backgroundImage = loadImage("Hot Air Ballon-01.png")
+  firebase.database
+}
+
+function draw() {
+  background(255,255,255);  
+  balloon.createSprite()
+  drawsprites();
+}
